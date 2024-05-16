8bb9490b52fa27396637c1c81dbc84cf6b23f148
santiagorch007 <112106750+santiagorch007@users.noreply.github.com>
2024-05-16 11:13:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+    name = os.environ.get("NAME", "Santiago, experto en Cloud Computing!")
+    return f"Hola {name}!"
 
 
 if __name__ == "__main__":
